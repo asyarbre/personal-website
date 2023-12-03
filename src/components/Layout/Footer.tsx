3601a50759556325f13549ee8c5eef67e970f006
Asya'ri <asyari818@gmail.com>
2023-12-03 14:01:35 +0700

    feat: add footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { IconType } from 'react-icons';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { Separator } from '../ui/separator';
+
+export default function Footer() {
+  return (
+    <footer className='mt-4 pb-4'>
+      <div className='layout flex flex-col items-center justify-center'>
+        <Separator className='my-2' />
+        <h4>Reach me out</h4>
+        <div className='flex items-center space-x-4'>
+          {socialLinks.map((socialLink) => (
+            <Link
+              key={socialLink.href}
+              href={socialLink.href}
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <socialLink.icon className='my-auto mt-3 h-6 w-6 hover:text-primary' />
+            </Link>
+          ))}
+        </div>
+        <p className='mt-4 text-muted-foreground'>
+          &copy; {new Date().getFullYear()} Asya'ri. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+const socialLinks: { href: string; icon: IconType }[] = [
+  {
+    href: 'https://www.linkedin.com/in/asyari1/',
+    icon: SiLinkedin,
+  },
+  {
+    href: 'https://github.com/asyarbre',
+    icon: SiGithub,
+  },
+];
