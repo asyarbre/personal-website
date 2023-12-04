2e6dc89dda2ad3fb658ef0fd704c41477df8e484
Asya'ri <asyari818@gmail.com>
2023-12-04 16:55:53 +0700

    refactor(seo): title

--- src/components/elements/Seo.tsx
+++ src/components/elements/Seo.tsx
@@ -24,19 +24,19 @@ export const Seo: React.FC<{
 
       {/* metadata */}
       <meta name='robots' content='follow, index' />
-      <meta name='title' content={title + ' | ' + defaultTitle} />
+      <meta name='title' content={title ? title + ' | ' + defaultTitle : defaultTitle} />
       <meta name='description' content={metaDescription} />
       <meta name='og:image' itemProp='image' content={ogImageUrl} />
       <meta property='og:url' content={pageUrl} />
 
       <meta property='og:type' content='website' />
       <meta property='og:image' itemProp='image' content={ogImageUrl} />
-      <meta property='og:title' content={title + ' | ' + defaultTitle} />
+      <meta property='og:title' content={title ? title + ' | ' + defaultTitle : defaultTitle} />
       <meta property='og:description' content={metaDescription} />
 
       <meta name='twitter:card' content='summary_large_image' />
       <meta property='twitter:url' content={pageUrl} />
-      <meta name='twitter:title' content={title + ' | ' + defaultTitle} />
+      <meta name='twitter:title' content={title ? title + ' | ' + defaultTitle : defaultTitle} />
       <meta name='twitter:image' content={ogImageUrl} />
       <meta property='twitter:description' content={metaDescription} />
 
