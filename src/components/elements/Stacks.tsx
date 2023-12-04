3d4e8fff3fe6762f88b486574a1a37b9d28de2fe
Asya'ri <asyari818@gmail.com>
2023-12-04 07:47:04 +0700

    refactor: edit data

--- src/components/elements/Stacks.tsx
+++ src/components/elements/Stacks.tsx
@@ -1,6 +1,6 @@
 import { BsFillBootstrapFill } from 'react-icons/bs';
 import {
-  SiCss3,
+  SiCodeigniter,
   SiDart,
   SiExpress,
   SiFirebase,
@@ -28,24 +28,24 @@ export type stacksProps = {
 const iconSize = 24;
 
 export const Stacks: stacksProps = {
-  PHP: <SiPhp size={iconSize} className='text-blue-500' />,
-  JavaScript: <SiJavascript size={iconSize} className='text-yellow-400' />,
-  TypeScript: <SiTypescript size={iconSize} className='text-blue-400' />,
   'Next.js': <SiNextdotjs size={iconSize} />,
   'React.js': <SiReact size={iconSize} className='text-sky-500' />,
+  JavaScript: <SiJavascript size={iconSize} className='text-yellow-400' />,
+  TypeScript: <SiTypescript size={iconSize} className='text-blue-400' />,
+  'Node.js': <SiNodedotjs size={iconSize} className='text-green-600' />,
+  Express: <SiExpress size={iconSize} />,
+  Prisma: <SiPrisma size={iconSize} className='text-emerald-500' />,
+  MySQL: <SiMysql size={iconSize} />,
+  MongoDB: <SiMongodb size={iconSize} className='text-green-500' />,
   TailwindCSS: <SiTailwindcss size={iconSize} className='text-cyan-300' />,
   Bootstrap: (
     <BsFillBootstrapFill size={iconSize} className='text-purple-500' />
   ),
+  PHP: <SiPhp size={iconSize} className='text-blue-500' />,
   Laravel: <SiLaravel size={iconSize} className='text-red-500' />,
+  Codeigniter: <SiCodeigniter size={iconSize} className='text-red-400' />,
   Vite: <SiVite size={iconSize} className='text-purple-500' />,
-  Prisma: <SiPrisma size={iconSize} className='text-emerald-500' />,
   Firebase: <SiFirebase size={iconSize} className='text-yellow-500' />,
-  'Node.js': <SiNodedotjs size={iconSize} className='text-green-600' />,
-  CSS: <SiCss3 size={iconSize} className='text-blue-300' />,
-  Express: <SiExpress size={iconSize} />,
-  MySQL: <SiMysql size={iconSize} />,
-  MongoDB: <SiMongodb size={iconSize} className='text-green-500' />,
   'Google Cloud': <SiGooglecloud size={iconSize} className='text-blue-500' />,
   Kotlin: <SiKotlin size={iconSize} className='text-purple-500' />,
   Flutter: <SiFlutter size={iconSize} className='text-blue-500' />,
