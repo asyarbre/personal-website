162e969f4871bd3521fa6af7ec328be1bfee7217
Asya'ri <asyari818@gmail.com>
2024-05-25 11:50:37 +0700

    refactor(skills): update skills section with marquee for better visual presentation

--- src/components/content/Home/Skills.tsx
+++ src/components/content/Home/Skills.tsx
@@ -3,6 +3,7 @@ import { ReactNode, useEffect, useState } from 'react';
 import InfiniteLoopSlider from '@/components/elements/InfiniteLoopSLider';
 import { Stacks } from '@/components/elements/Stacks';
 import { Separator } from '@/components/ui/separator';
+import Marquee from 'react-fast-marquee';
 
 const Tag = ({ icon, title }: { icon: ReactNode; title: string }) => (
   <div className='flex items-center gap-2 mr-3 rounded-full py-2 px-5 w-max bg-secondary border border-border'>
@@ -21,27 +22,21 @@ export default function Skills() {
     const shuffledArray = [...skillsArray].sort(() => Math.random() - 0.5);
     setShuffledSkills(shuffledArray);
   }, []);
-
-  const sliders = Array.from({ length: 3 }, (_, index) => {
-    const sliderSkills = [...shuffledSkills].sort(() => Math.random() - 0.5);
-    return (
-      <InfiniteLoopSlider key={index} isReverse={index === 1}>
-        {sliderSkills.map(([title, icon], index) => (
-          <Tag key={index} icon={icon} title={title} />
-        ))}
-      </InfiniteLoopSlider>
-    );
-  });
   return (
-    <section className='flex-col pb-20 justify-center'>
+    <section className='flex-col justify-center'>
       <article className='layout'>
         <h2 className='text-primary'>Skills</h2>
         <Separator className='my-2' />
-        <div className='flex w-full'>
-          <div className='relative flex flex-col gap-y-4 justify-start py-2 w-full overflow-hidden'>
-            {sliders}
-          </div>
-        </div>
+          <Marquee className='relative flex flex-col gap-y-4 justify-start py-2 w-full overflow-hidden'>
+            {shuffledSkills.map(([title, icon], index) => (
+              <Tag key={index} title={title} icon={icon} />
+            ))}
+          </Marquee>
+          <Marquee direction='right' className='relative flex flex-col gap-y-4 justify-start py-2 w-full overflow-hidden'>
+            {shuffledSkills.map(([title, icon], index) => (
+              <Tag key={index} title={title} icon={icon} />
+            ))}
+          </Marquee>
       </article>
     </section>
   );
