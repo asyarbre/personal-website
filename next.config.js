f580f58883e26ac0ce17c52e7123f376d7750328
Asya'ri <asyari818@gmail.com>
2024-05-25 11:49:53 +0700

    refactor: optimize image loading and add SEO description

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
+    unoptimized: true,
+  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
