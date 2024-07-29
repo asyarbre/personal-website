2e370f7982e55b63cb1691ff78c87acae53e91af
Asya'ri <asyari818@gmail.com>
2024-07-29 15:40:47 +0700

    refactor(ThemeProvider): update  with defaultTheme and enableSystem options

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/components/provider/theme-provider';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider attribute='class'>
+      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
