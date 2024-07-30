6f1e7ed3424be75c4b50d5fea40789c4e33f2bd9
Asya'ri <asyari818@gmail.com>
2024-07-30 16:09:55 +0700

    feat(about): add intro content

--- src/components/content/About/About.tsx
+++ src/components/content/About/About.tsx
@@ -1,7 +1,18 @@
+import { Bookmark } from 'lucide-react';
 import Image from 'next/image';
 
 import { Stacks } from '@/components/elements/Stacks';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
   Tooltip,
   TooltipContent,
@@ -9,12 +20,48 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 
+import Intro from './Intro';
+
 export default function About() {
   return (
-    <section className='layout min-h-main py-16'>
-      <h2>About</h2>
-      <h1 className='text-primary font-bold'>Asya'ri</h1>
-      <div className='mt-8'>
+    <section className='layout min-h-main my-8'>
+      <h2 className='text-primary'>About</h2>
+      <p className='text-muted-foreground'>
+        Deep insight into who I am—because every detail adds richness to the
+        tapestry of existence.
+      </p>
+      <Separator className='my-4' />
+
+      <Tabs defaultValue='intro' className='w-full'>
+        <TabsList className='grid w-full grid-cols-4'>
+          <TabsTrigger value='intro'>
+            <Bookmark size={14} className='mr-2' />
+            Intro
+          </TabsTrigger>
+          <TabsTrigger value='resume'>Resume</TabsTrigger>
+          <TabsTrigger value='career'>Career</TabsTrigger>
+          <TabsTrigger value='education'>Education</TabsTrigger>
+        </TabsList>
+        <TabsContent value='intro'>
+          <Intro />
+        </TabsContent>
+        <TabsContent value='resume'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you'll be logged out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='space-y-2'></CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+
+      <div className='mt-4'>
         <Image
           src='/images/profile.jpg'
           width='1000'
@@ -44,7 +91,7 @@ export default function About() {
         </article>
         <h3 className='mt-12 text-primary'>Tech Stack</h3>
         <figure className='mt-3 flex flex-wrap gap-3'>
-          <TooltipProvider>
+          <TooltipProvider delayDuration={50}>
             {Object.entries(Stacks).map(([text, icon], index) => (
               <Tooltip key={index}>
                 <TooltipTrigger>{icon}</TooltipTrigger>
---
+++ src/components/content/About/Intro.tsx
@@ -0,0 +1,53 @@
+import { Card, CardContent } from '@/components/ui/card';
+
+export default function Intro() {
+  return (
+    <Card>
+      <CardContent className='p-4 space-y-4 leading-[1.8] text-foreground md:leading-loose'>
+        <p>
+          I&apos;m an aspiring{' '}
+          <span className='font-semibold text-primary'>Software Engineer</span>{' '}
+          who is interested in{' '}
+          <span className='text-primary'>full-stack development.</span>
+        </p>
+        <p>
+          Passionate and skilled Full Stack Engineer with a strong foundation in
+          a diverse set of technologies. Proficient in front-end development
+          using ReactJS, NextJs, and mobile app development with Flutter and
+          Kotlin. Back-end expertise includes ExpressJs, Laravel, Codeigniter,
+          and database management with MySQL and MongoDB. Well-versed in
+          responsive design frameworks such as Bootstrap and TailwindCSS.
+          Additionally, possess Linux server administration skills and Google
+          Cloud Platform.
+        </p>
+        <p>
+          I am dedicated to creating user-friendly, visually appealing websites
+          and applications. My journey in software engineering has allowed me to
+          work on various projects, from individual contributions to leading
+          teams in developing innovative solutions. I thrive in environments
+          where collaboration and continuous learning are encouraged.
+        </p>
+        <p>
+          Throughout my career, I have collaborated with diverse teams to
+          deliver high-quality software solutions for both established
+          enterprises and emerging startups. My proficiency in JavaScript and
+          PHP, combined with my familiarity with frameworks like React and PHP
+          frameworks such as Codeigniter and Laravel, allows me to tackle
+          complex problems and innovate continuously.
+        </p>
+        <p>
+          I am a firm believer in the power of good communication and teamwork.
+          My experience has taught me the importance of understanding client
+          needs, leading projects to successful completion, and always striving
+          for excellence. I am dedicated to continuous learning and staying
+          updated with the latest advancements in web development and software
+          engineering.
+        </p>
+        <p>
+          I am excited about the opportunity to collaborate and bring creative
+          ideas to life. Let's work together to build amazing solutions!
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
