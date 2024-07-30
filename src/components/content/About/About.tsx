1b9e081a5e3b1331be433c217fb9e8bd389165fe
Asya'ri <asyari818@gmail.com>
2024-07-30 16:49:36 +0700

    refactor(about): update About component with Lucide icons

--- src/components/content/About/About.tsx
+++ src/components/content/About/About.tsx
@@ -1,4 +1,9 @@
-import { Bookmark } from 'lucide-react';
+import {
+  Bookmark,
+  Briefcase,
+  ClipboardList,
+  GraduationCap,
+} from 'lucide-react';
 import Image from 'next/image';
 
 import Intro from '@/components/content/About/Intro';
@@ -29,9 +34,18 @@ export default function About() {
             <Bookmark size={14} className='mr-2' />
             Intro
           </TabsTrigger>
-          <TabsTrigger value='resume'>Resume</TabsTrigger>
-          <TabsTrigger value='career'>Career</TabsTrigger>
-          <TabsTrigger value='education'>Education</TabsTrigger>
+          <TabsTrigger value='resume'>
+            <ClipboardList size={14} className='mr-2' />
+            Resume
+          </TabsTrigger>
+          <TabsTrigger value='career'>
+            <Briefcase size={14} className='mr-2' />
+            Career
+          </TabsTrigger>
+          <TabsTrigger value='education'>
+            <GraduationCap size={14} className='mr-2' />
+            Education
+          </TabsTrigger>
         </TabsList>
         <TabsContent value='intro'>
           <Intro />
