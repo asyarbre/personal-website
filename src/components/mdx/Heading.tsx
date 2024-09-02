9a75a4b29567e33663856d4d55864cccb48cbb92
Asya'ri <asyari818@gmail.com>
2024-09-02 09:46:04 +0700

    refactor(mdx-heading): update heading styles and classes

--- src/components/mdx/Heading.tsx
+++ src/components/mdx/Heading.tsx
@@ -1,8 +1,13 @@
 export const Heading = {
   H1: ({ children }: { children?: React.ReactNode }) => (
-    <h1 className='text-2xl font-bold'>{children}</h1>
+    <h1 className='text-2xl font-bold text-primary'>{children}</h1>
   ),
   H2: ({ children }: { children?: React.ReactNode }) => (
-    <h2 className='text-xl font-bold'>{children}</h2>
+    <h2 className='pt-4 text-xl font-bold text-foreground'>{children}</h2>
+  ),
+  H3: ({ children }: { children?: React.ReactNode }) => (
+    <h3 className='pt-4 text-[18px] font-medium leading-snug text-foreground'>
+      {children}
+    </h3>
   ),
 };
