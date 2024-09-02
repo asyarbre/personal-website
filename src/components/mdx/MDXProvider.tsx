95cb950e0f27d0af2f6ff5a3a242da61bbd19548
Asya'ri <asyari818@gmail.com>
2024-09-02 09:46:58 +0700

    feat: add UnorderedList component and update MDXProvider

--- src/components/mdx/MDXProvider.tsx
+++ src/components/mdx/MDXProvider.tsx
@@ -3,11 +3,14 @@ import { MDXProvider } from '@mdx-js/react';
 import CloudinaryImg from '@/components/elements/CloudinaryImage';
 import { Heading } from '@/components/mdx/Heading';
 import Para from '@/components/mdx/Para';
+import UnorderedList from '@/components/mdx/UnorderedList';
 
 const components = {
   h1: Heading.H1,
   h2: Heading.H2,
+  h3: Heading.H3,
   p: Para,
+  ul: UnorderedList,
   CloudinaryImg,
 };
 
